61ba6504a64c11a248d53950122584501dd082d4
rodrigoscher <79314511+rodrigoscher@users.noreply.github.com>
2023-06-08 21:09:05 -0300

    atualizando exercicios

--- 1-Formulas Matematicas_15/002-Media.js
+++ 1-Formulas Matematicas_15/002-Media.js
@@ -1,1 +1,9 @@
 // Escreva um programa que calcule a média aritmética de dois números.
+var num1 = 5;
+var num2 = 10;
+
+function media (num1, num2) {
+    return (num1 + num2)/2
+}
+
+console.log(media(num1, num2));
--- 1-Formulas Matematicas_15/003-MediaMais.js
+++ 1-Formulas Matematicas_15/003-MediaMais.js
@@ -1,1 +1,2 @@
 // Crie um programa que calcule e exiba a média aritmética de três notas informadas pelo usuário.
+
